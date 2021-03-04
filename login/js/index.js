82f1d51b688aa37850e53cb6682a4b5efd0b4d18
Patten Whiting <whi20045@byui.edu>
2021-03-04 10:41:39 -0700

    first commit

---
+++ login/js/index.js
@@ -0,0 +1,13 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQYdvg6KV-Iiozc7iR_3oLCSUvS7Y7Aok",
+    authDomain: "basic-login-a6782.firebaseapp.com",
+    projectId: "basic-login-a6782",
+    storageBucket: "basic-login-a6782.appspot.com",
+    messagingSenderId: "192317085965",
+    appId: "1:192317085965:web:3fa830d53a2cba882cb844",
+    measurementId: "G-8GBZNJ5G7G"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
