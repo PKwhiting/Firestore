cbdadacfbdd2a5e8fb494eac20ae669c71a85971
Patten Whiting <whi20045@byui.edu>
2021-03-05 15:08:59 -0700

    finished attempt #1

---
+++ README.md
@@ -0,0 +1,29 @@
+# Overview
+
+My current webpage is my first attempt at using a cloud databases for data mangament for a simple website. I am utilizing Firebase Firestore. The software I am trying to create is just a basic login and register functionality. Registering creates a new document within the user collection and ideally, logging in would query the database and verify that the username and password of a user logging in exists and is valid. The username and password are displayed after logging in or registering. Following registering or logging in, you can delete your account or update your account information. Currently I am running the frontend and backend of the JS all in one file. I would like to create full websites using node.js as the backend and using firebase as my database. Its ease of use and ability to write and read data fairly easy makes it an attractive database to use for my personal project. 
+
+
+[Software Demo Video](https://youtu.be/9k8W8bSJA1k)
+
+# Cloud Database
+
+Firebase Firestore
+
+
+# Development Environment
+
+VS Code
+Node.js
+HTML CSS LESS
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Stack Overflow](https://stackoverflow.com/)
+* [Firestore Documentation](https://firebase.google.com/docs/firestore)
+
+# Future Work
+
+* Use node.js as a framework to create a MVC model using JS for front and backend.
+* Create query and checking that verifies username and password.
+* Require authentication to access the DB.
--- login/js/index.js
+++ login/js/index.js
@@ -33,13 +33,18 @@ var all = document.getElementsByClassName('display');
 for (var i = 0; i < all.length; i++) {
   all[i].style.display = 'block';
 }
-document.getElementById("uname").innerHTML = username;
-document.getElementById("pword").innerHTML = password;
+
+var query = firestore.collection("users").where("user","==",username);
+query.get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+    field1 = doc.data().user;
+    field2 = doc.data().password;
+    document.getElementById("uname").innerHTML = field1;
+    document.getElementById("pword").innerHTML = field2;
+  });
+});
+
 }
-
-
-
-
 function registerview(){
  var all = document.getElementsByClassName('register');
  for (var i = 0; i < all.length; i++) {
@@ -55,9 +60,6 @@ function registerview(){
  }
 
  }
-
-
-
  function register(){
   fullname = document.getElementById("fullname").value;
   username = document.getElementById("email").value;
@@ -79,6 +81,104 @@ function registerview(){
  for (var i = 0; i < all.length; i++) {
    all[i].style.display = 'none';
  }
- document.getElementById("uname").innerHTML = username;
- document.getElementById("pword").innerHTML = password;
+ var query = firestore.collection("users").where("user","==",username);
+ query.get().then(function(querySnapshot) {
+   querySnapshot.forEach(function(doc) {
+     field1 = doc.data().user;
+     field2 = doc.data().password;
+     document.getElementById("uname").innerHTML = field1;
+     document.getElementById("pword").innerHTML = field2;
+   });
+ });
  }
+ function deleteuser(){
+  console.log('deleting');
+ username = document.getElementById("username").value;
+ password = document.getElementById("password").value;
+
+var query = firestore.collection("users").where("user","==",username);
+
+query.get().then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+    doc.ref.delete();
+  });
+});
+
+var all = document.getElementsByClassName('register');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('display');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('login');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'block';
+}
+ 
+
+}
+function modifyview(uname,pword){
+  uname = uname.innerHTML;
+  pword = pword.innerHTML;
+  var all = document.getElementsByClassName('register');
+  for (var i = 0; i < all.length; i++) {
+    all[i].style.display = 'none';
+  }
+  var all = document.getElementsByClassName('display');
+  for (var i = 0; i < all.length; i++) {
+    all[i].style.display = 'none';
+  }
+  var all = document.getElementsByClassName('login');
+  for (var i = 0; i < all.length; i++) {
+    all[i].style.display = 'none';
+  }
+  var all = document.getElementsByClassName('modify');
+  for (var i = 0; i < all.length; i++) {
+    all[i].style.display = 'block';
+  }
+  document.getElementById("ouname").innerHTML = uname;
+  document.getElementById("opword").innerHTML = pword;
+ 
+  }
+function modify(ouname,opword,uusername,upassword){
+  ouname = ouname.innerHTML;
+  opword = opword.innerHTML;
+  uusername = uusername.value;
+  upassword = upassword.value;
+  // var query = firestore.collection("users").where("user","==",uusername).limit(1).get();
+  // console.log(query);
+  // firestore.collection("users").doc(query).update({user:uusername, password:upassword});
+var query = firestore.collection("users");
+query.where("user","==",ouname).get()
+.then(function(querySnapshot) {
+  querySnapshot.forEach(function(doc) {
+      // doc.data() is never undefined for query doc snapshots
+      id = doc.id;
+      console.log(id);
+      query = firestore.collection("users").doc(id).update({user:uusername, password:upassword});
+  });
+})
+.catch(function(error) {
+  console.log("Error getting documents: ", error);
+});
+
+var all = document.getElementsByClassName('register');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('modify');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('display');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('login');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'block';
+}
+ 
+}
