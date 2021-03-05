536caba3ae240e51002e5551cdbf723ee9277be5
Patten Whiting <whi20045@byui.edu>
2021-03-05 10:27:15 -0700

    basic views and functionality complet

--- login/js/index.js
+++ login/js/index.js
@@ -1,5 +1,4 @@
-
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDQYdvg6KV-Iiozc7iR_3oLCSUvS7Y7Aok",
     authDomain: "basic-login-a6782.firebaseapp.com",
     projectId: "basic-login-a6782",
@@ -8,6 +7,78 @@
     appId: "1:192317085965:web:3fa830d53a2cba882cb844",
     measurementId: "G-8GBZNJ5G7G"
   };
-  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
+  const firestore = firebase.firestore();
+  const docRef = firestore.doc("users/user1");
+  const button = document.getElementById("submit");
+
+function login(){
+ username = document.getElementById("username").value;
+ password = document.getElementById("password").value;
+ console.log('test');
+
+ firestore.collection("users").doc().set({user: username, password: password
+  }).then(function(){console.log('Username logged!');
+  }).catch(function(error){console.log('error: ', error);
+ });
+
+
+ 
+ var all = document.getElementsByClassName('login');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'none';
+}
+var all = document.getElementsByClassName('display');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.display = 'block';
+}
+document.getElementById("uname").innerHTML = username;
+document.getElementById("pword").innerHTML = password;
+}
+
+
+
+
+function registerview(){
+ var all = document.getElementsByClassName('register');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'block';
+ }
+ var all = document.getElementsByClassName('display');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'none';
+ }
+ var all = document.getElementsByClassName('login');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'none';
+ }
+
+ }
+
+
+
+ function register(){
+  fullname = document.getElementById("fullname").value;
+  username = document.getElementById("email").value;
+  password = document.getElementById("rpassword").value;
+  console.log('test');
+  firestore.collection("users").doc().set({name: fullname, user: username, password: password, 
+   }).then(function(){console.log('User Registered!');
+   }).catch(function(error){console.log('error: ', error);
+  });
+  var all = document.getElementsByClassName('register');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'none';
+ }
+ var all = document.getElementsByClassName('display');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'block';
+ }
+ var all = document.getElementsByClassName('login');
+ for (var i = 0; i < all.length; i++) {
+   all[i].style.display = 'none';
+ }
+ document.getElementById("uname").innerHTML = username;
+ document.getElementById("pword").innerHTML = password;
+ }
